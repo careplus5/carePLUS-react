8a6439efe135e97c36f1224b8ede43270f2ea10a
샘 <108646100+HHansaem@users.noreply.github.com>
2024-05-30 17:17:14 +0900

    외래진료 환자정보 프론트

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Header from './resource/Header';
 import NurPatientList from './resource/NurPatientList';
 import Sidebar from './resource/Sidebar';
 import Login from './resource/Login';
+import DiagnosisPatient from './resource/DiagnosisPatient';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Sidebar/>
-      <NurPatientList/>
-      <Login/>
+      <DiagnosisPatient/>
+      {/* <NurPatientList/> */}
+      {/* <Login/> */}
     </div>
   );
 }

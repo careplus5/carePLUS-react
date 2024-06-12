7ebb33ffd8d8988bae87094284c71bf6085212be
yebrung <officeguider@naver.com>
2024-06-12 14:33:27 +0900

    sur

--- src/resource/SurgeryPatient.js
+++ src/resource/SurgeryPatient.js
@@ -7,7 +7,7 @@ import SurgeryWrite from './SurgeryWrite';
 const SurgeryPatient = () => {
 
     return (
-        <div className="background" style={{marginTop:'-10px', paddingLeft:'35px'}}>
+        <div className="background">
             <div id="firstRow" style={{height: "340px"}}>
                 <div id="sboxLeft">
                     <div className="diagBoxHeader" style={{position:"sticky"}}>

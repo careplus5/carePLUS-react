c122285bfbe0ac9690ba304455d42befb714159d
60keem <164153439+60keem@users.noreply.github.com>
2024-06-19 11:08:44 +0900

    안냐세요 프로필 저장합니다
    수고링~

--- src/resource/SideProfile.js
+++ src/resource/SideProfile.js
@@ -1,40 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../css/SideProfile.css';
-import { useAtom, useAtomValue } from 'jotai';
-import { accessTokenAtom, empAtom, usernameAtom} from '../config/Atom.js';
-const SideProfile = () => {
-    const empName = useAtomValue(empAtom);
-    const [profileData, setProfileData] = useState(null);
-    const username = useAtomValue(usernameAtom);
+import { url } from '../config';
+import { useAtomValue } from 'jotai';
+import { usernameAtom } from '../config/Atom';
 
-    // useEffect(()=>{
-    //     axios.get('/empName')
-    //     .then(res=>{
-    //      console.log(res);
-    //     })
-    //     .catch(err=>{
-    //      console.log(err);
-    //     })
-    // },[username])
+const SideProfile = () => {
+    const [profileData, setProfileData] = useState(null);
+    const userId = useAtomValue(usernameAtom);
+
     useEffect(() => {
-        const sampleProfileData = {
-            profileImg: '../img/profileImg.png',
-            name: empName.empName+" 님",
-            department: '어쩌구부',
-            department2: '저쩌구과'
-    };    
-        // setProfileData로 상태를 설정해야 함
-        setProfileData(sampleProfileData);
-        
-    }, [username]);
+        const fetchUserInfo = async () => {
+            try {
+                const res = await axios.get(`${url}/userInfo?userId=${userId}`);
+                const { empName, departmentName, department2Name, profNum } = res.data;
+                const ProfileData = {
+                    profileImg: `${url}/image/${profNum}`,
+                    name: `${empName} 님`,
+                    department: departmentName,
+                    department2: department2Name,
+                };
+                setProfileData(ProfileData);
+                console.log(res);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchUserInfo();
+    }, [userId]);
 
     return (
         <div className="side-profile">
-            <br/>
+            <br />
             {profileData && (
                 <>
-                    <img id="profileImg" src={profileData.profileImg} alt="Profile" /><br/>
+                    <img id="profileImg" src={profileData.profileImg} alt="Profile" /><br />
                     <p className="profile-name">{profileData.name}</p>
                     <span className='profile-dept1'>{profileData.department}</span>
                     <span className='profile-dept2'>{profileData.department2}</span>

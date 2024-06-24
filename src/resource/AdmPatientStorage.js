9c60fcae7ff1b2c6bf6ccff2756a8ac3a8c016f1
샘 <108646100+HHansaem@users.noreply.github.com>
2024-06-24 16:22:10 +0900

    호

--- src/resource/AdmPatientStorage.js
+++ src/resource/AdmPatientStorage.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { url } from '../config';
 import AdmDiagCheckModal from './AdmDiagCheckModal';
 import AdmDischargeCheckModal from './AdmDischargeCheckModal';
+import '../css/Adm.css';
 
 // 기타 문서 발급
 const AdmPatientStorage = ({ patient }) => {
@@ -70,102 +71,111 @@ const AdmPatientStorage = ({ patient }) => {
     return (
         <div id="LaccordionBox">
             <div>
-                <div className="boxHeader" style={{ marginLeft: "35px" }} >
-                    <img id="boxIcon" style={{ width: "40px", height: "40px" }} src="./img/document.png" />&nbsp;
-                    <h3 id="LboxHeader" style={{ marginTop: "19px", marginRight: "125px" }}>기타 문서 발급</h3>
+                <div className="LboxHeader" style={{ display: 'flex', margin:'15px 25px' }}>
+                    <img id="boxIcon" style={{ marginTop: "15px", marginLeft: "15px" }} src="./img/notice.png" />
+                    <h3 className="admPat-boxHeader">기타문서발급</h3>
                 </div>
-                <br /><br />
-                <div style={{marginLeft:'150px'}}>
+                <div style={{marginLeft:'145px'}}>
                     <span >환자번호</span>
                     <input type="text" name='patNum' value={patient && patient.patNum}
-                        className='admInputType' style={{marginLeft: "10px", width: "200px", height: "30px"}} />
-                    <span style={{ marginLeft: "21px" }}>주민등록번호</span>
+                        className='inputStyle' style={{width:"90px"}} />
+                    <span style={{ marginLeft: "20px" }}>주민등록번호</span>
                     <input type="text" value={patient && patient.patJumin}
-                        className='admInputType' style={{marginLeft: "10px", width: "200px", height: "30px"}} />
-                    <span style={{ marginLeft: "21px" }}>이름</span>
+                        className='inputStyle'/>
+                    <span style={{ marginLeft: "20px" }}>이름</span>
                     <input type="text" value={patient && patient.patName}
-                        className='admInputType' style={{marginLeft: "10px", width: "200px", height: "30px"}} />
-                    <button onClick={() => getPatDiagCheckList()} style={{ backgroundColor:'#0081b4', marginLeft: '20px', height: '35px' }}>진료확인서</button>
-                    <button onClick={() => getPatAdmCheckList()} style={{ backgroundColor:'#0081b4', marginLeft: '20px', height: '35px' }}>입·퇴원확인서</button>
+                        className='inputStyle' style={{width:"90px"}}/>
+                    <span style={{ marginLeft: "20px" }}>성별</span>
+                    <input type="text" value={patient && patient.patGender}
+                        className='inputStyle' style={{width:"45px"}}/>
+                    <span style={{ marginLeft: "20px" }}>전화번호</span>
+                    <input type="text" value={patient && patient.patTel}
+                        className='inputStyle'/>
+                    <button onClick={() => getPatDiagCheckList()} className='buttonStyle' style={{marginLeft:'40px'}}>진료확인서</button>
+                    <button onClick={() => getPatAdmCheckList()} className='buttonStyle'>입·퇴원확인서</button>
                     <br /><br />
                     {/* 진료확인서 */}
                     {showTable === 'diagCheck' && patient && (
-                        <Table bordered style={{ maxHeight:'300px', overflowY:'auto', marginLeft:'-75px', textAlign: 'center', backgroundColor:'#fbf9f2' }}>
-                            <thead>
-                                <tr>
-                                    <th>환자번호</th>
-                                    <th>이름</th>
-                                    <th>진료과</th>
-                                    <th>담당의사번</th>
-                                    <th>담당의</th>
-                                    <th>검사종류</th>
-                                    <th>초진일자</th>
-                                    <th>진단일자</th>
-                                    <th>진료종류</th>
-                                    <th>발급여부</th>
-                                    <th>진료확인서</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {patDiagCheckList.map(diagCheck => 
-                                <tr key={diagCheck.docDiagnosisNum}>
-                                    <td>{diagCheck.patNum}</td>
-                                    <td>{diagCheck.patName}</td>
-                                    <td>{diagCheck.deptName}</td>
-                                    <td>{diagCheck.docNum}</td>
-                                    <td>{diagCheck.docName}</td>
-                                    <td>{diagCheck.testName === null ? '-' : diagCheck.testName}</td>
-                                    <td>{diagCheck.firstDiagDate}</td>
-                                    <td>{diagCheck.docDiagnosisDate}</td>
-                                    <td>
-                                        {diagCheck.docDiagnosisKind === 'diag' ? '외래' : 
-                                        diagCheck.docDiagnosisKind === 'adm' ? '입원' :
-                                        diagCheck.docDiagnosisKind}
-                                    </td>
-                                    <td>미발급</td>
-                                    <td><button style={{ backgroundColor: 'black' }} onClick={() => openDiagCheckModal(diagCheck)}>진료확인서 출력</button></td>
-                                </tr>)}
-                            </tbody>
-                        </Table>
+                        <div className='admPatStorageScroll'>
+                            <Table bordered style={{ textAlign: 'center', backgroundColor:'#fbf9f2' }}>
+                                <thead>
+                                    <tr>
+                                        <th>환자번호</th>
+                                        <th>이름</th>
+                                        <th>진료과</th>
+                                        <th>담당의사번</th>
+                                        <th>담당의</th>
+                                        <th>검사종류</th>
+                                        <th>초진일자</th>
+                                        <th>진단일자</th>
+                                        <th>진료종류</th>
+                                        <th>발급여부</th>
+                                        <th>진료확인서</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {patDiagCheckList.map(diagCheck => 
+                                    <tr key={diagCheck.docDiagnosisNum}>
+                                        <td>{diagCheck.patNum}</td>
+                                        <td>{diagCheck.patName}</td>
+                                        <td>{diagCheck.deptName}</td>
+                                        <td>{diagCheck.docNum}</td>
+                                        <td>{diagCheck.docName}</td>
+                                        <td>{diagCheck.testName === null ? '-' : diagCheck.testName}</td>
+                                        <td>{diagCheck.firstDiagDate}</td>
+                                        <td>{diagCheck.docDiagnosisDate}</td>
+                                        <td>
+                                            {diagCheck.docDiagnosisKind === 'diag' ? '외래' : 
+                                            diagCheck.docDiagnosisKind === 'adm' ? '입원' :
+                                            diagCheck.docDiagnosisKind}
+                                        </td>
+                                        <td>미발급</td>
+                                        <td><button style={{ backgroundColor: 'black' }} onClick={() => openDiagCheckModal(diagCheck)}>진료확인서</button></td>
+                                    </tr>)}
+                                </tbody>
+                            </Table>
+                        </div>
                     )}
                     {/* 입·퇴원확인서 */}
                     {showTable === 'admCheck' && patient && (
-                        <Table bordered style={{ textAlign: 'center', marginLeft:'-75px'}}>
-                            <thead>
-                                <tr>
-                                    <th>환자번호</th>
-                                    <th>이름</th>
-                                    <th>입원부서</th>
-                                    <th>담당의사번</th>
-                                    <th>담당의</th>
-                                    <th>간호사사번</th>
-                                    <th>담당간호사</th>
-                                    <th>입원일자</th>
-                                    <th>퇴원일자</th>
-                                    <th>입원상태</th>
-                                    <th>입·퇴원확인서</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {patAdmCheckList.map(AdmCheck => 
-                                <tr key={AdmCheck.admissionNum}>
-                                    <td>{AdmCheck.patNum}</td>
-                                    <td>{AdmCheck.patName}</td>
-                                    <td>{AdmCheck.deptName}</td>
-                                    <td>{AdmCheck.docNum}</td>
-                                    <td>{AdmCheck.docName}</td>
-                                    <td>{AdmCheck.nurNum}</td>
-                                    <td>{AdmCheck.nurName}</td>
-                                    <td>{AdmCheck.admissionDate}</td>
-                                    <td>{AdmCheck.admissionDischargeDate}</td>
-                                    <td>{AdmCheck.admissionStatus === 'ing' ? '입원중' : 
-                                        AdmCheck.admissionStatus === 'end' ? '퇴원' :
-                                        AdmCheck.admissionStatus}
-                                    </td>
-                                    <td><button style={{ backgroundColor: 'black' }} onClick={() => openAdmCheckModal(AdmCheck)}>입·퇴원확인서 출력</button></td>
-                                </tr>)}
-                            </tbody>
-                        </Table>
+                        <div className='admPatStorageScroll'>
+                            <Table bordered style={{ textAlign: 'center'}}>
+                                <thead>
+                                    <tr>
+                                        <th>환자번호</th>
+                                        <th>이름</th>
+                                        <th>입원부서</th>
+                                        <th>담당의사번</th>
+                                        <th>담당의</th>
+                                        <th>간호사사번</th>
+                                        <th>담당간호사</th>
+                                        <th>입원일자</th>
+                                        <th>퇴원일자</th>
+                                        <th>입원상태</th>
+                                        <th>입·퇴원확인서</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {patAdmCheckList.map(AdmCheck => 
+                                    <tr key={AdmCheck.admissionNum}>
+                                        <td>{AdmCheck.patNum}</td>
+                                        <td>{AdmCheck.patName}</td>
+                                        <td>{AdmCheck.deptName}</td>
+                                        <td>{AdmCheck.docNum}</td>
+                                        <td>{AdmCheck.docName}</td>
+                                        <td>{AdmCheck.nurNum}</td>
+                                        <td>{AdmCheck.nurName}</td>
+                                        <td>{AdmCheck.admissionDate}</td>
+                                        <td>{AdmCheck.admissionDischargeDate}</td>
+                                        <td>{AdmCheck.admissionStatus === 'ing' ? '입원중' : 
+                                            AdmCheck.admissionStatus === 'end' ? '퇴원' :
+                                            AdmCheck.admissionStatus}
+                                        </td>
+                                        <td><button style={{ backgroundColor: 'black' }} onClick={() => openAdmCheckModal(AdmCheck)}>입·퇴원확인서</button></td>
+                                    </tr>)}
+                                </tbody>
+                            </Table>
+                        </div>
                     )}
                 </div>
             </div>
